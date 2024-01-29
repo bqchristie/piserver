9aec5f64c0ff2a8f5f0cb81278efea3cdf9fd9ba
Bruce Christie <bqchristie@gmail.com>
2024-01-29 15:47:41 -0500

    updating error

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.post('/gpio/blink', function(req, res) {
 
         var LED = new Gpio(4, 'out'); //use GPIO pin 4, and specify that it is output
         var blinkInterval = setInterval(blinkLED, 250); //run the blinkLED function every 250ms
-        
+
 
         function blinkLED() { //function to start blinking
             if (LED.readSync() === 0) { //check the pin state, if the state is 0 (or off)
